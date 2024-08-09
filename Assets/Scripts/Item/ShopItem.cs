a0f808346d9bbd3b7932a6180d05eb326232a84f
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-08-09 20:48:53 +0530

    ShopItem Updated

--- Assets/Scripts/Item/ShopItem.cs
+++ Assets/Scripts/Item/ShopItem.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
+using TMPro; 
 public class ShopItem : MonoBehaviour,IPointerClickHandler
 {
+
     public int itemID;
     public string itemName;
+    public int itemQuantity;
     public Image itemImage;
     public List<ShopItem> shopItemList;
+    public int buyingPrice;
+    public TextMeshProUGUI buyingPriceText; 
+    public TextMeshProUGUI itemQuantityText; 
 
     private void Start()
     {
-        itemImage.sprite = InventoryService.Instance.GetInventoryView().inventoryController.inventoryModel.itemSOList.InventoryItems[itemID].ItemSprite;
+       // itemImage.sprite = InventoryService.Instance.GetInventoryView().inventoryController.inventoryModel.itemSOList.InventoryItems[itemID].ItemSprite;
+        itemQuantity = 99;
+        itemQuantityText.text = itemQuantity.ToString();
+        buyingPriceText.text = InventoryService.Instance.GetInventoryView().inventoryController.inventoryModel.itemSOList.InventoryItems[itemID].ItemSellingPrice.ToString();
     }
     public void OnPointerClick(PointerEventData eventData)
     {
